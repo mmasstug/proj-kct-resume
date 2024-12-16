63bdfa074da4d6012f3db4ebb954670f958a75dc
mmasstug <daniillapin258@gmail.com>
2024-12-16 21:28:49 +0500

    handler

--- cmd/main.go
+++ cmd/main.go
@@ -5,16 +5,26 @@ import (
 	"github.com/mmasstug/proj-kct-resume/pkg/handler"
 	"github.com/mmasstug/proj-kct-resume/pkg/repository"
 	"github.com/mmasstug/proj-kct-resume/pkg/servise"
+	"github.com/spf13/viper"
 	"log"
 )
 
 func main() {
+	if err := initConfig(); err != nil {
+		log.Fatalf("error initializing configs: %s", err.Error())
+	}
 	repos := repository.NewReposirory()
 	servise := servise.NewService(repos)
 	handlers := handler.NewHendler(servise)
 
 	srv := new(proj.Server)
-	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
+	if err := srv.Run(viper.GetString("8000"), handlers.InitRoutes()); err != nil {
 		log.Fatalf("error running: %s", err.Error())
 	}
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
